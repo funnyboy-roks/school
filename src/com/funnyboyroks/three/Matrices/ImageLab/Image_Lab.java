2d6984c4c014fdd81d62e9993bb4bd84551632f1
funnyboy-roks <funnyboyroks@gmail.com>
2021-08-25 14:49:03 -0500

    Add "useful" comments

--- src/com/funnyboyroks/three/Matrices/ImageLab/Image_Lab.java
+++ src/com/funnyboyroks/three/Matrices/ImageLab/Image_Lab.java
@@ -18,47 +18,51 @@ class Image_Lab {
         g.setColor(Color.BLACK);
         g.fillOval(10, 100, 20, 70);
 
+        // Draw cyan/green rect on bottom of panel
         g.setColor(Color.CYAN);
         g.fillRect(0, panel.getHeight() - 50, panel.getWidth(), 50);
         g.setColor(Color.GREEN);
         g.drawRect(0, panel.getHeight() - 50, panel.getWidth(), 50);
 
-		mirror(panel);
-		negative(panel);
+        mirror(panel);
+        negative(panel);
         rotateClockwise(panel);
     }
 
     //Flips the pixels of the given drawing panel horizontally
     public static void mirror(DrawingPanel panel) {
         Color[][] pixels = panel.getPixels();
-        /**Insert your code here!
-         Think of "pixels" as a normal matrix
-         */
+        // Loop through each row and reverse it
         for (int i = 0; i < pixels.length; i++) {
-            reverseArr(pixels[i]);
+            reverseArray(pixels[i]);
         }
 
         panel.setPixels(pixels);
     }
 
-    public static void reverseArr(Color[] arr) {
-        Color[] t = arr.clone();
-        for (int i = 1; i < arr.length + 1; ++i) {
-            arr[arr.length - i] = t[i - 1];
+    /**
+     * Reverses an array
+     *
+     * @param array The array to reverse
+     */
+    public static void reverseArray(Color[] array) {
+        Color[] t = array.clone();
+        for (int i = 1; i < array.length + 1; ++i) {
+            array[array.length - i] = t[i - 1];
         }
     }
 
     //Rotates the image clockwise by 90 degrees(a quarter turn)
     public static void rotateClockwise(DrawingPanel panel) {
         Color[][] pixels = panel.getPixels();
-        Color[][] out = new Color[pixels.length][pixels[0].length];
+        Color[][] out = new Color[pixels.length][pixels[0].length]; // A temp holding spot that will be used to set the new array
 
         for (int y = 0; y < pixels.length; y++) {
-            Color[] row = pixels[y];
+            Color[] row = pixels[y]; // Single row in Matrix
             for (int x = 0; x < row.length; x++) {
-                Color pixel = row[x];
+                Color pixel = row[x]; // Single pixel item
                 if (x <= pixels.length && y <= pixels[0].length) {
-                    out[x][y] = pixel;
+                    out[x][y] = pixel; // Se the pixel
                 }
             }
         }
@@ -73,13 +77,13 @@ class Image_Lab {
         //Remember that every pixel is a Color object that has
         //a Red, Blue and Green Color value.
         for (int i = 0; i < pixels.length; i++) {
-            Color[] row = pixels[i];
+            Color[] row = pixels[i]; // The row
             for (int j = 0; j < row.length; j++) {
-                Color px = row[j];
+                Color px = row[j]; // The pixel (used for colour info 🠗)
                 int r = 255 - px.getRed();
                 int g = 255 - px.getGreen();
                 int b = 255 - px.getBlue();
-                row[j] = new Color(r, g, b);
+                row[j] = new Color(r, g, b); // Set the new colour
             }
         }
 

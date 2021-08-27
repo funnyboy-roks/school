e11d715cb086eda9b7166d80463d4975a51b6ac4
funnyboy-roks <funnyboyroks@gmail.com>
2021-08-27 14:46:39 -0500

    Apply Formatting

--- src/com/funnyboyroks/three/Matrices/PascalsTriangle/PascalsTriangle.java
+++ src/com/funnyboyroks/three/Matrices/PascalsTriangle/PascalsTriangle.java
@@ -8,19 +8,33 @@ import java.util.Arrays;
 public class PascalsTriangle {
 
     public static void main(String[] args) {
-        int height = 10;
 
-        int[][] mat = new int[height][];
-        mat[0] = new int[]{1};
-
-        for (int i = 1; i < mat.length - 1; i++) {
-            mat[i] = getNext(mat[i - 1]);
-        }
-
-        System.out.println(Arrays.deepToString(mat));
-
+        int[][] mat = pascal(5);
+        System.out.println(prettify(mat));
     }
 
+    /**
+     * Generate pascal's triangle in a matrix
+     *
+     * @param rows The amount of rows
+     * @return The output matrix
+     */
+    public static int[][] pascal(int rows) {
+        int[][] mat = new int[rows][];
+        mat[0] = new int[]{1};
+
+        for (int i = 1; i < mat.length; i++) {
+            mat[i] = getNext(mat[i - 1]);
+        }
+        return mat;
+    }
+
+    /**
+     * Get the next step in the triangle
+     *
+     * @param prev The preview layer
+     * @return The next layer
+     */
     public static int[] getNext(int... prev) {
         int[] out = new int[prev.length + 1];
         int p = 0; // Store previous value
@@ -30,5 +44,25 @@ public class PascalsTriangle {
             p = n2;
         }
         return out;
+    }
+
+    /**
+     * Prettify the 2d matrix to make it a triangle
+     * @param mat The matrix
+     * @return The formatted String
+     */
+    public static String prettify(int[][] mat) {
+        int padAmt = mat[mat.length-1].length;
+        StringBuilder sb = new StringBuilder();
+        for (int[] row : mat) {
+            sb.append(" ".repeat(padAmt));
+            for (int i : row) {
+                sb.append(i).append(' ');
+            }
+            sb.append(" ".repeat(padAmt));
+            padAmt--;
+            sb.append('\n');
+        }
+        return sb.toString();
     }
 }

cb0f42315032a32a560a9487da116b4bf0427b21
funnyboy-roks <funnyboyroks@gmail.com>
2021-08-31 14:48:13 -0500

    Write Advanced Matrix Rotations

---
+++ src/com/funnyboyroks/three/Matrices/Rotation/AdvancedMatrixRotation.java
@@ -0,0 +1,152 @@
+package com.funnyboyroks.three.Matrices.Rotation;
+
+import java.util.Arrays;
+
+public class AdvancedMatrixRotation {
+
+    public static void main(String[] args) {
+
+        int[][] mat = {
+            {0, 1, 2, 3},
+            {4, 5, 6, 7},
+            {8, 9, 0, 1},
+            {2, 3, 4, 5},
+            };
+
+        rotate(mat);
+
+        System.out.println(Arrays.deepToString(mat));
+
+    }
+
+    /**
+     * Rotate the full matrix (Recursive)
+     *
+     * @param mat The matrix to rotate(Must be square)
+     */
+    public static void rotate(int[][] mat) {
+        if (mat.length != mat[0].length) throw new IllegalArgumentException("mat must be a square matrix");
+        int[] values = getBorder(mat);
+        setBorder(mat, rotate(values));
+        if (mat.length == 2) return;
+        int[][] sub = subMatrix(mat);
+        rotate(sub);
+        setSubMatrix(mat, sub);
+//        System.out.println(Arrays.toString(values));
+    }
+
+    /**
+     * Get the matrix that is one level in
+     *
+     * @param mat The parent matrix
+     * @return The output matrix
+     */
+    public static int[][] subMatrix(int[][] mat) {
+        int[][] out = new int[mat.length - 2][mat.length - 2];
+        int oy = 0;
+        int ox = 0;
+        for (int y = 1; y < mat.length - 1; y++) {
+            ox = 0;
+            for (int x = 1; x < mat.length - 1; x++) {
+                out[ox][oy] = mat[x][y];
+                ++ox;
+            }
+            ++oy;
+        }
+        return out;
+    }
+
+    /**
+     * Get the matrix that is one level in
+     *
+     * @param mat The parent matrix
+     * @return The output matrix
+     */
+    public static void setSubMatrix(int[][] mat, int[][] sub) {
+        int oy = 0;
+        int ox = 0;
+        for (int y = 1; y < mat.length - 1; y++) {
+            ox = 0;
+            for (int x = 1; x < mat.length - 1; x++) {
+                mat[x][y] = sub[ox][oy];
+                ++ox;
+            }
+            ++oy;
+        }
+    }
+
+    /**
+     * Rotate the values of a 1D array
+     *
+     * @param arr The array to rotate
+     * @return The rotated array
+     */
+    public static int[] rotate(int[] arr) {
+        int[] out = new int[arr.length]; // Output array
+        out[0] = arr[arr.length - 1];
+        for (int i = 0; i < arr.length - 1; i++) {
+            out[i + 1] = arr[i];
+        }
+        return out;
+    }
+
+    /**
+     * Get the border items from the matrix
+     *
+     * @param mat The matrix
+     * @return The border items
+     */
+    public static int[] getBorder(int[][] mat) {
+
+        int[] values = new int[mat.length * 2 + (mat.length - 2) * 2];
+        int pos = 0; // The position to add new values
+        for (int n : mat[0]) {
+            values[pos] = n;
+            ++pos;
+        }
+        for (int i = 1; i < mat.length - 1; ++i) {
+            values[pos] = mat[i][mat[i].length - 1];
+            ++pos;
+        }
+        int[] lastRow = mat[mat.length - 1];
+        for (int i = lastRow.length - 1; i >= 0; i--) {
+            values[pos] = lastRow[i];
+            pos++;
+
+        }
+        for (int i = mat.length - 2; i >= 1; i--) {
+            values[pos] = mat[i][0];
+            ++pos;
+        }
+        return values;
+    }
+
+    /**
+     * Set the border items of the matrix
+     *
+     * @param mat    The matrix
+     * @param values The new border items to use
+     */
+    public static void setBorder(int[][] mat, int[] values) {
+
+        int pos = 0; // The position to read new values
+        for (int i = 0; i < mat[0].length; i++) {
+            mat[0][i] = values[pos];
+            ++pos;
+        }
+        for (int i = 1; i < mat.length - 1; ++i) {
+            mat[i][mat[i].length - 1] = values[pos];
+            ++pos;
+        }
+        int[] lastRow = mat[mat.length - 1];
+        for (int i = lastRow.length - 1; i >= 0; i--) {
+            lastRow[i] = values[pos];
+            pos++;
+
+        }
+        for (int i = mat.length - 2; i >= 1; i--) {
+            mat[i][0] = values[pos];
+            ++pos;
+        }
+    }
+}

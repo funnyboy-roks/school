14e11a991f4cf201e0e2b730dbd64dd17c7e8d9f
funnyboy-roks <funnyboyroks@gmail.com>
2022-09-19 14:10:20 -0500

    Program1

---
+++ program1.py
@@ -0,0 +1,7 @@
+name = input('Please enter your name: ')
+
+test1 = int(input(f'Hi {name}. What did you score on your first test? '))
+test2 = int(input('What did you score on your second test? '))
+
+print(f'The average of {test1} and {test2} is {(test1 + test2) / 2}')
+

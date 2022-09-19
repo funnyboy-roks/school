b18b345e79cdee7b50dc068951228cc55e138296
funnyboy-roks <funnyboyroks@gmail.com>
2022-09-19 14:19:26 -0500

    Fix program 1

--- program1.py
+++ program1.py
@@ -1,7 +1,16 @@
+################################################################################
+# Name: Hayden Pott
+# Date: 19 Sept 2022
+# Description: Calculuate the average between two test scores read from user input
+################################################################################
+
+# a statement that prompts the user for his name
 name = input('Please enter your name: ')
 
+# statement(s) that prompts the user for two test scores.
 test1 = int(input(f'Hi {name}. What did you score on your first test? '))
 test2 = int(input('What did you score on your second test? '))
 
+# display the final output
 print(f'The average of {test1} and {test2} is {(test1 + test2) / 2}')
 

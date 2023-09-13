d9b9461498242521688679c9878a185db5f784da
funnyboy-roks <funnyboyroks@gmail.com>
2023-09-13 08:19:08 -0500

    Add q1/program1

---
+++ Q1/program1/HelloAutograder.java
@@ -0,0 +1,17 @@
+/**
+ * Name:   Hayden Pott
+ * Date:   8 September 2023
+ * Course: CSC 220 - Data Structures
+ */
+
+public class HelloAutograder extends AbstractHelloAutograder {
+
+    /**
+    * Returns the String "Hello Autograder!"
+    * @return "Hello Autograder!"
+    */
+    @Override
+    public String sayHello() {
+        return "Hello Autograder!";
+    }
+}

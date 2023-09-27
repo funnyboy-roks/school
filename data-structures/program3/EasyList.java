74277bae15094e141521da0ee52f431845cca831
funnyboy-roks <funnyboyroks@gmail.com>
2023-09-27 12:50:49 -0500

    Unspoken requirements are the devil's spawn

--- data-structures/program3/EasyList.java
+++ data-structures/program3/EasyList.java
@@ -12,20 +12,18 @@ public class EasyList extends AbstractEasyList {
 	 */
 	public EasyList() {
         this.list = new char[EasyList.MAX_SIZE];
-        this.end = 0;
+        this.end = -1;
 	}
 
 	/**
 	 * Copy constructor - constructs an EasyList whose elements are copied from
 	 * the given EasyList
-	 * @param		el EasyList to copy from
+	 * @param		other EasyList to copy from
 	 */
-	public EasyList(EasyList el) {
-        this.end = el.size();
+	public EasyList(EasyList other) {
+        this.end = other.end;
         this.list = new char[EasyList.MAX_SIZE];
-        for(int i = this.end; --i >= 0;) {
-            this.list[i] = el.get(i);
-        }
+        System.arraycopy(other.list, 0, this.list, 0, this.end + 1);
 	}
 
 	/**
@@ -33,7 +31,7 @@ public class EasyList extends AbstractEasyList {
 	 * @return 		number of elements in list
 	 */
 	public int size() {
-		return this.end;
+		return this.end + 1;
 	}
 
 	/**
@@ -42,14 +40,15 @@ public class EasyList extends AbstractEasyList {
 	 */
     @Override
 	public String toString() {
-        if (this.end != 0) {
-            String out = "";
-            for(int i = 0; i < this.end; ++i) {
-                out += this.list[i] + " ";
-            }
-            return out;
+        if (this.end == -1) {
+            return "NULL";
         }
-        return "NULL";
+
+        String out = "";
+        for(int i = 0; i <= this.end; ++i) {
+            out += this.list[i] + " ";
+        }
+        return out;
 	}
 
 	/**
@@ -58,8 +57,8 @@ public class EasyList extends AbstractEasyList {
 	 * @param 		element the element to be added to the list
 	 */
 	public void add(char elt) {
-        if (this.end < this.list.length) {
-            this.list[this.end++] = elt;
+        if (this.end + 1 < this.list.length) {
+            this.list[++this.end] = elt;
         }
 	}
 
@@ -71,20 +70,20 @@ public class EasyList extends AbstractEasyList {
 	 * @param 		element the element to be added to the list
 	 */
 	public void add(int index, char elt) {
-        if (index == this.end) {
+        if (index == this.end + 1) {
             this.add(elt);
-        } else if (index >= 0 && this.end < this.list.length && index <= this.end) {
-            System.arraycopy(this.list, index, this.list, index + 1, this.end++ - index);
+        } else if (0 <= index && index <= this.end + 1 && this.end + 1 < this.list.length) {
+            System.arraycopy(this.list, index, this.list, index + 1, ++this.end - index);
             this.list[index] = elt;
         }
 	}
 
     /**
-     * Check if an index is valid and within the bounds of the current list (compares with `this.end`)
+     * Check if an index is valid and within the bounds of the current list (compares with `(this.end + 1)`)
      * @return wheter it is a valid index
      */
     private boolean withinBounds(int index) {
-        return index >= 0 && index < this.end;
+        return 0 <= index && index < this.end + 1;
     }
 
 	/**
@@ -104,7 +103,7 @@ public class EasyList extends AbstractEasyList {
 	 */
 	public void remove(int index) {
         if (withinBounds(index)) {
-            System.arraycopy(this.list, index + 1, this.list, index, this.end-- - index);
+            System.arraycopy(this.list, index + 1, this.list, index, this.end-- + 1 - index);
         }
 	}
  
@@ -127,8 +126,8 @@ public class EasyList extends AbstractEasyList {
 	 * @return 		the index of element, or -1 if it doesn't exist
 	 */
 	public int indexOf(char elt) {
-        for (int i = this.end; --i >= 0;) {
-            if (elt  == this.list[i]) {
+        for (int i = this.end + 1; --i >= 0;) {
+            if (elt == this.list[i]) {
                 return i;
             }
         }
@@ -140,7 +139,7 @@ public class EasyList extends AbstractEasyList {
 	 */
 	public void sort() {
         char c;
-        for (int i = 1; i < this.end; ++i) {
+        for (int i = 1; i <= this.end; ++i) {
             for (int j = i; j > 0 && this.list[j - 1] > this.list[j]; this.list[--j] = c) {
                 c = this.list[j];
                 this.list[j] = this.list[j - 1];
@@ -148,11 +147,19 @@ public class EasyList extends AbstractEasyList {
         }
 	}
 
+	/**
+	 * Returns true if there are no elements in the list
+	 * @return 		true if list is empty, false otherwise
+	 */
     public boolean isEmpty() {
-        return this.end == 0;
+        return this.end == -1;
     }
 
+	/**
+	 * Returns true if the number of elements in the list is equal to MAX_SIZE
+	 * @return 		true if list is full, false otherwise
+	 */
     public boolean isFull() {
-        return this.end == this.list.length;
+        return this.end + 1 == EasyList.MAX_SIZE;
     }
 }
--- data-structures/program3/Test.java
+++ data-structures/program3/Test.java
@@ -1,10 +1,53 @@
+import java.util.*;
+
 public class Test {
     private static EasyList l;
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        //testCopyConstructor();
+
+        testAll();
+
+        //testSort();
+    }
+
+    public static void testSort() {
+        EasyList l = new EasyList();
+        //AbstractEasyList prof = new EasyListSolution();
+        AbstractEasyList student = new EasyList();
+        Random r = new Random();
+
+        // Create array list of letters from A to Z
+        ArrayList<Character> letters = new ArrayList<Character>();
+        for (char c = 'A'; c != '['; c++) letters.add(c);
+        // Fill the prof list with randomly selected letters
+        for (int i = 0; i < 10; i++) student.add(letters.remove(r.nextInt(letters.size())));
+
+        // Copy the prof list into the student list
+        //student.secretCopy(prof);
+
+        // Run the test
+        System.out.println("The unsorted list: " + student);
+        //prof.sort();
+
+        //System.out.println("My sorted list: " + prof.toString());
+        student.sort();
+
+        System.out.println("Your sorted list:  " + student);
+
+        //assertEquals(prof.toString(), student.toString());
+    }
+
+    public static void testAll() {
         testConstructor();
-        testToString();
+        System.out.println("Test empty: expect: true   found: " + l.isEmpty());
+        System.out.println("Test full:  expect: false  found: " + l.isFull());
+        System.out.println("contents: " + l);
+        System.out.println("adding items");
         testAdd();
-        testToString();
+        System.out.println("Test empty: expect: false  found: " + l.isEmpty());
+        System.out.println("Test full:  expect: false  found: " + l.isFull());
+        System.out.println("contents: " + l);
+
         System.out.printf("Size = %d\n", l.size());
         l.add(0, '0');
         testToString();
@@ -12,13 +55,21 @@ public class Test {
         l.add(6, '6');
         testToString();
         System.out.printf("Size = %d\n", l.size());
+
         testGet();
         testRemove();
         System.out.println("Test copy constructor");
+        System.out.print("before: ");
+        testToString();
         l = new EasyList(l);
+        System.out.print("after: ");
         testToString();
         l.set(1, '5');
         testToString();
+
+        for (int i = 10; --i >= 0;) l.add('.');
+        System.out.println("Test full:  expect: true   found: " + l.isFull());
+        System.out.println("contents: " + l);
 
         System.out.println("New list");
         l = new EasyList();
@@ -35,24 +86,55 @@ public class Test {
         System.out.printf("indexOf('a') = %s\n", l.indexOf('a'));
         System.out.printf("indexOf('e') = %s\n", l.indexOf('e'));
         System.out.printf("indexOf('g') = %s\n", l.indexOf('g'));
-	}
+    }
 
-	public static void testConstructor() {
+    public static void testCopyConstructor() {
+        EasyList student = new EasyList();
+        var r = new Random();
+        ArrayList<Character> letters = new ArrayList<Character>();
+        for (char c = 'A'; c != '['; c++) letters.add(c);
+        for (char c = 'A'; c != '['; c++) letters.add(c);
+        for (char c = 'A'; c != '['; c++) letters.add(c);
+        for (char c = 'A'; c != '['; c++) letters.add(c);
+        //for (int i = 0; i < 10; i++) prof.add();
+
+        for (int i = 50; --i >= 0;) student.add(letters.remove(r.nextInt(letters.size())));
+
+        //student.secretCopy(prof);
+
+        // Test student copy constructor
+        AbstractEasyList studentCopy = new EasyList(student);
+        System.out.println("The original list: " + student);
+        System.out.println("Your copied list:  " + studentCopy);
+        // Must contain the same elements
+        //assertEquals(prof.toString(), studentCopy.toString());
+        System.out.printf("%s == \n", studentCopy);
+        // Must have the same end index
+        //assertEquals(prof.size(), studentCopy.size());
+        System.out.printf("%s == \n", studentCopy.size());
+        //System.out.println();
+    }
+
+    public static void testConstructor() {
         l = new EasyList();
-	}
+    }
 
-	public static void testToString() {
+    public static void testToString() {
         System.out.println(l);
-	}
+    }
 
-	public static void testAdd() {
+    public static void testAdd() {
         l.add('a');
+        System.out.println("Add 'a' -> " + l);
         l.add('b');
+        System.out.println("Add 'b' -> " + l);
         l.add('c');
+        System.out.println("Add 'c' -> " + l);
         l.add('d');
-	}
+        System.out.println("Add 'd' -> " + l);
+    }
 
-	public static void testGet() {
+    public static void testGet() {
         System.out.printf("l[0] = %s\n", l.get(0));
         System.out.printf("l[1] = %s\n", l.get(1));
         System.out.printf("l[2] = %s\n", l.get(2));
@@ -63,22 +145,10 @@ public class Test {
         System.out.printf("l[7] = %s\n", l.get(7));
         System.out.printf("l[8] = %s\n", l.get(8));
         System.out.printf("l[9] = %s\n", l.get(9));
-	}
+    }
 
-	public static void testRemove() {
+    public static void testRemove() {
         l.remove(3);
         System.out.printf("Removed 3: %s\n", l);
-	}
-
-	public static void testSet() {
-		
-	}
-
-	public static void testIndexOf() {
-		
-	}
-
-	public static void testSort() {
-		
-	}
+    }
 }
